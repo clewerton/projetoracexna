03aa587e8d1582406f8861685d1ec4869c5a6cc9
h.anjos <h.anjos@c0dbee6d-f4a8-26d3-2708-f81666f7118e>
2012-07-19 03:52:09 +0000

    Criando um arquivo separado para Transition.

---
+++ trunk/road rage/WindowsGame1/WindowsGame1/WindowsGame1/States/Transition.cs
@@ -0,0 +1,33 @@
+﻿namespace TangoGames.RoadFighter.States
+{
+    public class Transition<TState, TInput>
+    {
+        public Transition(TState state, TInput input)
+        {
+            State = state;
+            Input = input;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+
+            var other = obj as Transition<TState, TInput>;
+            return other != null && object.Equals(State, other.State) && object.Equals(Input, other.Input);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (State.GetHashCode() * 397) ^ Input.GetHashCode();
+            }
+        }
+
+        public TState State { get; private set; }
+        public TInput Input { get; private set; }
+    }
+
+    
+}
