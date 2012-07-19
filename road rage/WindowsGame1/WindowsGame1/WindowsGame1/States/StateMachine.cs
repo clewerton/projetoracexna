1f5f8e83fea4bd9c4bf8921fca068897678b743a
h.anjos <h.anjos@c0dbee6d-f4a8-26d3-2708-f81666f7118e>
2012-07-19 03:52:09 +0000

    Criando um arquivo separado para Transition.

--- road rage/WindowsGame1/WindowsGame1/WindowsGame1/States/StateMachine.cs
+++ road rage/WindowsGame1/WindowsGame1/WindowsGame1/States/StateMachine.cs
@@ -1,38 +1,7 @@
-﻿
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace TangoGames.RoadFighter.States
 {
-    public class Transition<TState, TInput>
-    {
-        public Transition(TState state, TInput input)
-        {
-            State = state;
-            Input = input;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-
-            var other = obj as Transition<TState, TInput>;
-            return other != null && object.Equals(State, other.State) && object.Equals(Input, other.Input);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return (State.GetHashCode() * 397) ^ Input.GetHashCode();
-            }
-        }
-
-        public TState State { get; private set; }
-        public TInput Input { get; private set; }
-    }
-
     public class StateMachine<TState, TInput> : Dictionary<Transition<TState, TInput>, TState>
     {
         public StateMachine(TState initial)
