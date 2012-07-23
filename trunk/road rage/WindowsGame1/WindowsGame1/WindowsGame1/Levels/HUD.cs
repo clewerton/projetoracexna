db61be5f610dc7f799ff3e41f70e32f078bc4e07
honorato.gangsta <honorato.gangsta@c0dbee6d-f4a8-26d3-2708-f81666f7118e>
2012-07-23 21:48:51 +0000

    ajustada a classe HUD.cs e adicionada a textura do heroi

--- trunk/road rage/WindowsGame1/WindowsGame1/WindowsGame1/Levels/HUD.cs
+++ trunk/road rage/WindowsGame1/WindowsGame1/WindowsGame1/Levels/HUD.cs
@@ -15,9 +15,9 @@ namespace TangoGames.RoadFighter.Levels
     {
         public int pontos = 0;     // total de pomntos do player
         public float gasolina = 100; // quantidade de gasolina do herói
-        float porcentagemGasolina = 100;
         float angulacaoGasolina = 90;
         Single radianos;
+       
 
         Texture2D indicadorCombustivel;  // imagem do indicador de gasolina
         Texture2D ponteiro;                // ponteiro do indicador de gasolina
@@ -29,9 +29,9 @@ namespace TangoGames.RoadFighter.Levels
 
         public HUD(ContentManager Content)
         {
-
-            Arial = Content.Load<SpriteFont>("arial");
             
+            Arial = Content.Load<SpriteFont>("arial");
+                        
             marcadorPontos = Content.Load<Texture2D>("HUDElementos/Pontos");
             indicadorCombustivel = Content.Load<Texture2D>("HUDElementos/gasolina");
             ponteiro = Content.Load<Texture2D>("HUDElementos/ponteiro");
@@ -41,8 +41,7 @@ namespace TangoGames.RoadFighter.Levels
             
         {
 
-            calculagasolina(gameTime);
-            calculapontos(gameTime);
+            calculaparametros(gameTime);
 
             ponteirogasolina(gameTime);
         }
@@ -52,47 +51,37 @@ namespace TangoGames.RoadFighter.Levels
             
         {
             
-            //SpriteBatch.Begin();
-
             spriteBatch.DrawString(Arial, " " +pontos, new Vector2(100, 45), Color.Red);
 
             spriteBatch.Draw(marcadorPontos, new Rectangle(0, 10, 204, 53),new Rectangle(18,10,204,53) , Color.White);
             spriteBatch.Draw(indicadorCombustivel, new Rectangle(0, 90, 171, 233), new Rectangle(18, 10, 171, 205), Color.White);
             spriteBatch.Draw(ponteiro, new Rectangle(35,205, 78, 6), null, Color.White, radianos, new Vector2(0, ponteiro.Height/2), SpriteEffects.None, 0);
 
-            //spriteBatch.Draw(ponteiro, new Rectangle(indicadorCombustivel.Width / 6, 70 + indicadorCombustivel.Height / 2, 100, 36), new Rectangle(0, 0, 72, 9), Color.White, 0, new Vector2(0, 0), SpriteEffects.None, 0);
+            
         }
 
 
-        void calculagasolina(GameTime gametime) // calcula a quantidade de gasolina gasta
-        {
-
-
-        }
 
         void ponteirogasolina(GameTime gametime) // calcula a angulacao do ponteiro
         {
             
 
-            angulacaoGasolina = (-90 * gasolina) / 100;
+            angulacaoGasolina = (-90 * gasolina) / 100; // calcula o angulo do ponteiro em relacao a quantidade de gasolina
 
-            //1.57
-
-            angulacaoGasolina = angulacaoGasolina + 45;
-
-            //angulacaoGasolina = -45;
-
-            radianos = (((float)Math.PI * angulacaoGasolina) / 180);
            
 
+            angulacaoGasolina = angulacaoGasolina + 45; // ajusta o valor do angulo em relacao ao desenho do ponteiro
+
             
+
+            radianos = (((float)Math.PI * angulacaoGasolina) / 180);       // converte de graus para radianos 
 
 
         }
 
 
 
-        void calculapontos(GameTime gametime) // calcula a quantidade de pontos ganhos
+        void calculaparametros(GameTime gametime) // calcula a quantidade de pontos ganhos
         {
             
             
@@ -107,17 +96,13 @@ namespace TangoGames.RoadFighter.Levels
             {
                 tempodecorrido = 0;
                 contadordePontos += 1;
+
                 gasolina --;
             }
 
             pontos = contadordePontos * 10;
 
-            //if ((tempodecorrido % 1) == 0)
-            //{
-                //pontos += 10;
-            //}
-            //}
-
+            
             if (gasolina <= 0)
             {
 
