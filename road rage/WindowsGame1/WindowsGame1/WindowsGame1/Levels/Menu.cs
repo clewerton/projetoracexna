6a9d045af0652baa34f60c4652b08710d238de47
h.anjos <h.anjos@c0dbee6d-f4a8-26d3-2708-f81666f7118e>
2012-08-02 01:02:04 +0000

    A cena de Menu agora tem uma imagem de fundo.

--- road rage/WindowsGame1/WindowsGame1/WindowsGame1/Levels/Menu.cs
+++ road rage/WindowsGame1/WindowsGame1/WindowsGame1/Levels/Menu.cs
@@ -12,19 +12,22 @@ namespace TangoGames.RoadFighter.Levels
 
         protected override void LoadContent()
         {
+            // configurando o fundo
+            BackgroundImage = Game.Content.Load<Texture2D>("Textures/menu-background");
+
             // configurando os botões da tela
             var clientBounds = Game.Window.ClientBounds;
 
             ToIntro = new Button(Game);
             ToIntro.OnClick += (sender, args) => GetService<ISceneManagerService<MainGame.Scenes>>().GoTo(MainGame.Scenes.Intro);
-            ToIntro.Location = new Point((clientBounds.Width - ButtonWidth) / 2, (clientBounds.Height - ButtonHeight) / 2 - ButtonHeight - Padding);
+            ToIntro.Location = new Point((clientBounds.Width - ButtonWidth) / 2, (clientBounds.Height - ButtonHeight) / 2 - 2 * ButtonHeight - 2 * Padding);
             ToIntro.Size = new Vector2(ButtonWidth, ButtonHeight);
             ToIntro.Text = "To INTRO";
             Elements.Add(ToIntro);
 
             ToFase = new Button(Game);
             ToFase.OnClick += (sender, args) => GetService<ISceneManagerService<MainGame.Scenes>>().GoTo(MainGame.Scenes.Fase);
-            ToFase.Location = new Point((clientBounds.Width - ButtonWidth) / 2, (clientBounds.Height - ButtonHeight) / 2);
+            ToFase.Location = new Point((clientBounds.Width - ButtonWidth) / 2, (clientBounds.Height - ButtonHeight) / 2 - ButtonHeight - Padding);
             ToFase.Size = new Vector2(ButtonWidth, ButtonHeight);
             ToFase.Text = "To FASE";
             ToFase.Texture = Game.Content.Load<Texture2D>("Widgets/Button");
@@ -32,21 +35,21 @@ namespace TangoGames.RoadFighter.Levels
 
             ToEnd = new Button(Game);
             ToEnd.OnClick += (sender, args) => GetService<ISceneManagerService<MainGame.Scenes>>().GoTo(MainGame.Scenes.End);
-            ToEnd.Location = new Point((clientBounds.Width - ButtonWidth) / 2, (clientBounds.Height + ButtonHeight) / 2 + Padding);
+            ToEnd.Location = new Point((clientBounds.Width - ButtonWidth) / 2, (clientBounds.Height - ButtonHeight) / 2);
             ToEnd.Size = new Vector2(ButtonWidth, ButtonHeight);
             ToEnd.Text = "To END";
             Elements.Add(ToEnd);
 
             ToCredits = new Button(Game);
             ToCredits.OnClick += (sender, args) => GetService<ISceneManagerService<MainGame.Scenes>>().GoTo(MainGame.Scenes.Credits);
-            ToCredits.Location = new Point((clientBounds.Width - ButtonWidth) / 2, clientBounds.Height / 2 + 3 * ButtonHeight / 2 + 2 * Padding);
+            ToCredits.Location = new Point((clientBounds.Width - ButtonWidth) / 2, (clientBounds.Height - ButtonHeight) / 2 + ButtonHeight + Padding);
             ToCredits.Size = new Vector2(ButtonWidth, ButtonHeight);
             ToCredits.Text = "To CREDITS";
             Elements.Add(ToCredits);
 
             ToExit = new Button(Game);
             ToExit.OnClick += (sender, args) => Game.Exit();
-            ToExit.Location = new Point((clientBounds.Width - ButtonWidth) / 2, clientBounds.Height / 2 + 5 * ButtonHeight / 2 + 3 * Padding);
+            ToExit.Location = new Point((clientBounds.Width - ButtonWidth) / 2, (clientBounds.Height - ButtonHeight) / 2 + 2 * ButtonHeight + 2 * Padding);
             ToExit.Size = new Vector2(ButtonWidth, ButtonHeight);
             ToExit.Text = "EXIT";
             Elements.Add(ToExit);
@@ -55,13 +58,19 @@ namespace TangoGames.RoadFighter.Levels
         protected override void DrawBefore(GameTime gameTime)
         {
             Game.GraphicsDevice.Clear(Color.Brown);
+
+            SpriteBatch.Begin();
+            SpriteBatch.Draw(BackgroundImage, Game.Window.ClientBounds, Color.White);
+            SpriteBatch.End();
         }
 
-        public Button ToIntro { get; private set; }
-        public Button ToFase { get; private set; }
-        public Button ToEnd { get; private set; }
-        public Button ToCredits { get; private set; }
-        public Button ToExit { get; private set; }
+        private Button ToIntro { get; set; }
+        private Button ToFase { get; set; }
+        private Button ToEnd { get; set; }
+        private Button ToCredits { get; set; }
+        private Button ToExit { get; set; }
+
+        private Texture2D BackgroundImage { get; set; }
 
         private const int ButtonWidth = 150;
         private const int ButtonHeight = 60;
