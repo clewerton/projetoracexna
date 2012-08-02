4296be1bde01390d0142b947fa8e857664a53497
h.anjos <h.anjos@c0dbee6d-f4a8-26d3-2708-f81666f7118e>
2012-08-02 00:34:48 +0000

    Criando um serviço responsável por manter estado que deverá ser acessado e manipulado por múltiplas cenas.

---
+++ trunk/road rage/WindowsGame1/WindowsGame1/WindowsGame1/Shared/SharedState.cs
@@ -0,0 +1,13 @@
+﻿namespace TangoGames.RoadFighter.Shared
+{
+    /// <summary>
+    /// Um serviço que mantém estado compartilhado por múltiplas cenas.
+    /// </summary>
+    public interface ISharedState
+    {
+    }
+
+    public class SharedState : ISharedState
+    {
+    }
+}
