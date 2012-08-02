71558050794f8580d4105ff800ae1185273007f5
h.anjos <h.anjos@c0dbee6d-f4a8-26d3-2708-f81666f7118e>
2012-08-02 00:15:50 +0000

    Colocando um fundo para Intro e mudando para Menu quando se clicar na cena.

--- trunk/road rage/WindowsGame1/WindowsGame1/WindowsGame1/Levels/Intro.cs
+++ trunk/road rage/WindowsGame1/WindowsGame1/WindowsGame1/Levels/Intro.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Graphics;
+using TangoGames.RoadFighter.Input;
 using TangoGames.RoadFighter.Scenes;
 using System;
 using TangoGames.RoadFighter.Widgets;
@@ -9,26 +10,21 @@ namespace TangoGames.RoadFighter.Levels
 {
     public class Intro : Scene
     {
-        public Intro(Game game) : base(game) {}
+        public Intro(Game game) : base(game)
+        {
+            _timeElapsed = TimeSpan.Zero;   
+        }
 
         protected override void LoadContent()
         {
             _arial = Game.Content.Load<SpriteFont>("arial");
-            _timeElapsed = TimeSpan.Zero;
 
-            _button = new Button(Game);
-            _button.Location = new Point(200, 300);
-            _button.Size = new Vector2(120, 60);
-            _button.Text = "To END";
-
-            _button.OnClick +=
-                (sender, args) =>
-                {
-                    var sceneManager = GetService<ISceneManagerService<MainGame.Scenes>>();
-
-                    sceneManager.GoTo(MainGame.Scenes.End);
-                };
-            Elements.Add(_button);
+            BackgroundImage = Game.Content.Load<Texture2D>("Textures/intro-background");
+            
+            Text = new TextArea(Game);
+            Text.Text = "Clique em qualquer lugar para começar!";
+            Text.Font = _arial;
+            Elements.Add(Text);
         }
 
         public override void Update(GameTime gameTime)
@@ -36,8 +32,9 @@ namespace TangoGames.RoadFighter.Levels
             _timeElapsed += gameTime.ElapsedGameTime;
 
             var sceneManager = GetService<ISceneManagerService<MainGame.Scenes>>();
+            var input = GetService<IInputService>();
         
-            if(Keyboard.GetState().IsKeyDown(Keys.Enter))
+            if(input.MouseClick(Game.Window.ClientBounds)) // se clicou dentro da tela, vá para o menu
             {
                sceneManager.GoTo(MainGame.Scenes.Menu);
                return;
@@ -49,21 +46,25 @@ namespace TangoGames.RoadFighter.Levels
         protected override void DrawBefore(GameTime gameTime)
         {
             Game.GraphicsDevice.Clear(Color.Azure);
-        }
 
-        protected override void DrawAfter(GameTime gameTime)
-        {
             SpriteBatch.Begin();
-            SpriteBatch.DrawString(_arial, "In INTRO; press ENTER to go to MENU", new Vector2(100), Color.BurlyWood);
-            SpriteBatch.DrawString(_arial, "In INTRO; press SPACE to " + (Paused ? "resume" : "pause"), new Vector2(100, 130), Color.BurlyWood);
-            SpriteBatch.DrawString(_arial, "Time: " + _timeElapsed, new Vector2(100, 160), Color.BurlyWood);
+            SpriteBatch.Draw(BackgroundImage, Game.Window.ClientBounds, Color.White);
             SpriteBatch.End();
+
+            // posicionando o texto
+            var screenWidth = Game.Window.ClientBounds.Width;
+            var screenHeight = Game.Window.ClientBounds.Height;
+
+            Text.Location = new Point((int) (screenWidth - Text.Size.X)/2, (int) (screenHeight - Text.Size.Y)/2);
         }
 
         #region Properties & Fields
+
+        private Texture2D BackgroundImage { get; set; }
+        private TextArea Text { get; set; }
+
         private SpriteFont _arial;
         private TimeSpan _timeElapsed;
-        private Button _button;
         #endregion
     }
 }
