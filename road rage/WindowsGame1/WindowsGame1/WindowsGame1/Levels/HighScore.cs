02784f08b42969e4af1dfed3f391fc3176e41e2d
clewerton.coelho <clewerton.coelho@c0dbee6d-f4a8-26d3-2708-f81666f7118e>
2012-08-06 02:47:36 +0000

    Implementado teclado virtual.

--- road rage/WindowsGame1/WindowsGame1/WindowsGame1/Levels/HighScore.cs
+++ road rage/WindowsGame1/WindowsGame1/WindowsGame1/Levels/HighScore.cs
@@ -12,9 +12,16 @@ namespace TangoGames.RoadFighter.Levels
 {
     class HighScore : Scene
     {
+        private String[] characters;
 
         public HighScore(Game game) : base(game) 
         {
+            characters = new String[4];
+            characters[0] = "0123456789";
+            characters[1] = "ABCDEFGHIJ";
+            characters[2] = "KLMNOPQRST";
+            characters[3] = "UVWXYZ";
+
             startX = game.Window.ClientBounds.Width / 4;
             startY = game.Window.ClientBounds.Height / 2;
         }
@@ -23,17 +30,53 @@ namespace TangoGames.RoadFighter.Levels
         {
             // configurando o fundo
             BackgroundImage = Game.Content.Load<Texture2D>("Textures/menu-background");
+            font = Game.Content.Load<SpriteFont>("arial");
+            playerName = "";
 
             // configurando os botões da tela
             var clientBounds = Game.Window.ClientBounds;
 
-            Button letterA = new Button(Game);
-            letterA.OnClick += (sender, args) => playerName += 'A';
-            letterA.Bounds = new Rectangle(0, 0, 40, 40);
-            letterA.Location = new Point((clientBounds.Width - ButtonWidth) / 2, (clientBounds.Height - ButtonHeight) / 2 - 2 * ButtonHeight - 2 * Padding);
-            letterA.Size = new Vector2(ButtonWidth, ButtonHeight);
-            letterA.Text = "[A]";
-            Elements.Add(letterA);
+            int i, j;
+            for (i = 0; i < characters.Length; i++)
+            {
+                for (j = 0; j < characters[i].Length; j++)
+                {
+                    Button letterButton = new Button(Game);
+                    Char currentLetter = characters[i].ElementAt(j);
+                    letterButton.OnClick += (sender, args) => playerName += currentLetter;
+                    letterButton.Bounds = new Rectangle(0, 0, ButtonWidth, ButtonWidth);
+                    letterButton.Location = new Point(startX + 50 * j + Padding, startY + 50 * i + Padding);
+                    letterButton.Size = new Vector2(ButtonWidth, ButtonHeight);
+                    letterButton.Text = "[" + currentLetter + "]";
+                    Elements.Add(letterButton);
+                }
+            }
+
+            // Tecla de espaço
+            Button spaceButton = new Button(Game);
+            Char space = ' ';
+            spaceButton.OnClick += (sender, args) => playerName += space;
+            spaceButton.Bounds = new Rectangle(0, 0, ButtonWidth, ButtonWidth);
+            spaceButton.Location = new Point(startX + 50 * 6 + Padding, startY + 50 * 3 + Padding);
+            spaceButton.Size = new Vector2(ButtonWidth, ButtonHeight);
+            spaceButton.Text = "[" + space + "]";
+            Elements.Add(spaceButton);
+
+            // Tecla de Backspace
+            Button bckButton = new Button(Game);
+            Char back = '<';
+            bckButton.OnClick += (sender, args) =>
+            {
+                if (playerName.Length > 0)
+                {
+                    playerName = playerName.Remove(playerName.Length - 1);
+                }
+            };
+            bckButton.Bounds = new Rectangle(0, 0, ButtonWidth, ButtonWidth);
+            bckButton.Location = new Point(startX + 50 * 7 + Padding, startY + 50 * 3 + Padding);
+            bckButton.Size = new Vector2(ButtonWidth, ButtonHeight);
+            bckButton.Text = "[" + back + "]";
+            Elements.Add(bckButton);
 
         }
 
@@ -56,17 +99,25 @@ namespace TangoGames.RoadFighter.Levels
             Game.GraphicsDevice.Clear(Color.Brown);
 
             SpriteBatch.Begin();
+
+            Vector2 size = font.MeasureString(playerName);
+            var location = new Vector2(
+                startX + (ButtonWidth - size.X) / 2,
+                startY - 100 + (ButtonHeight - size.Y) / 2
+            );
+
             SpriteBatch.Draw(BackgroundImage, Game.Window.ClientBounds, Color.White);
+            SpriteBatch.DrawString(font, playerName, location, Color.White);
             SpriteBatch.End();
         }
 
         private Texture2D BackgroundImage { get; set; }
-        private const int ButtonWidth = 40;
-        private const int ButtonHeight = 40;
+        private const int ButtonWidth = 50;
+        private const int ButtonHeight = 50;
         private const int Padding = 5;
         private String playerName;
         private int startX;
         private int startY;
-
+        private SpriteFont font;
     }
 }
